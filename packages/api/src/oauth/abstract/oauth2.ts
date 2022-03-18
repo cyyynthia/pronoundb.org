e4127dc59283f67550db9a67504a97bfef1b1d14
Cynthia <cynthia@cynthia.dev>
2022-03-18 16:26:48 +0100

    chore: add spdx-license-identifier

--- packages/api/src/oauth/abstract/oauth2.ts
+++ packages/api/src/oauth/abstract/oauth2.ts
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020-2022 Cynthia K. Rey, All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
