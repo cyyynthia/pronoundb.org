ee4ea058538c344316b284ee7aed0c6cb60fb07d
Cynthia <cynthia@cynthia.dev>
2023-01-05 23:37:04 +0100

    chore: drop license year
    no more yearly commit to change the year

--- packages/website/src/components/pages/DocsLegacy.tsx
+++ packages/website/src/components/pages/DocsLegacy.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022 Cynthia K. Rey, All rights reserved.
+ * Copyright (c) Cynthia Rey, All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  *
  * Redistribution and use in source and binary forms, with or without
