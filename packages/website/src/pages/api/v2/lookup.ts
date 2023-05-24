b452773602ad893be62b7a44b2fd725287ef5b91
Cynthia <cynthia@cynthia.dev>
2023-05-24 15:01:29 +0200

    feat: implement APIv2 routes

---
+++ packages/website/src/pages/api/v2/lookup.ts
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) Cynthia Rey et al., All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import type { APIContext } from 'astro'
+
+import { LookupRequestsCounter, LookupIdsCounter, LookupHitCounter, LookupBulkSizeHistogram, ApiCallVersionCounter } from '@server/metrics.js'
+import { findPronounsOf } from '@server/database/account.js'
+import { providers } from '@server/oauth/providers.js'
+
+export async function get (ctx: APIContext) {
+	ApiCallVersionCounter.inc({ version: 2 })
+
+	const platform = ctx.url.searchParams.get('platform')
+	const idsStr = ctx.url.searchParams.get('ids')
+
+	if (!platform || !idsStr) {
+		return new Response(
+			JSON.stringify({
+				code: 400,
+				error: 'Bad request',
+				message: '`platform` and `ids` query parameters are required.',
+			}),
+			{
+				status: 400,
+				headers: {
+					'access-control-allow-methods': 'GET',
+					'access-control-allow-origin': '*',
+					'access-control-allow-headers': 'x-pronoundb-source',
+					'access-control-max-age': '600',
+					'content-type': 'application/json',
+				},
+			}
+		)
+	}
+
+	if (!providers.includes(platform)) {
+		return new Response(
+			JSON.stringify({
+				code: 400,
+				error: 'Bad request',
+				message: '`platform` is not a valid platform.',
+			}),
+			{
+				status: 400,
+				headers: {
+					'access-control-allow-methods': 'GET',
+					'access-control-allow-origin': '*',
+					'access-control-allow-headers': 'x-pronoundb-source',
+					'access-control-max-age': '600',
+					'content-type': 'application/json',
+				},
+			}
+		)
+	}
+
+	const ids = new Set(idsStr.split(',').filter((a) => a))
+	const idsCount = ids.size
+	if (idsCount < 1 || idsCount > 50) {
+		return new Response(
+			JSON.stringify({
+				code: 400,
+				error: 'Bad request',
+				message: '`ids` must contain between 1 and 50 IDs.',
+			}),
+			{
+				status: 400,
+				headers: {
+					'access-control-allow-methods': 'GET',
+					'access-control-allow-origin': '*',
+					'access-control-allow-headers': 'x-pronoundb-source',
+					'access-control-max-age': '600',
+					'content-type': 'application/json',
+				},
+			}
+		)
+	}
+
+	const cursor = findPronounsOf(platform, Array.from(ids))
+
+	let idsHitCount = 0
+	const res = Object.create(null)
+	for await (const user of cursor) {
+		idsHitCount++
+		res[user.account.id] = {
+			decoration: user.decoration,
+			sets: user.sets,
+		}
+	}
+
+	// Collect metrics
+	const method = idsCount === 1 ? 'single' : 'bulk'
+	LookupRequestsCounter.inc({ platform: platform, method: method })
+	LookupIdsCounter.inc({ platform: platform }, idsCount)
+	LookupHitCounter.inc({ platform: platform }, idsHitCount)
+	if (method === 'bulk') {
+		LookupBulkSizeHistogram.observe({ platform: platform }, idsCount)
+	}
+
+	const body = JSON.stringify(res)
+	return new Response(body, {
+		headers: {
+			'access-control-allow-methods': 'GET',
+			'access-control-allow-origin': '*',
+			'access-control-allow-headers': 'x-pronoundb-source',
+			'access-control-max-age': '600',
+			'content-type': 'application/json',
+		},
+	})
+}
+
+export function options () {
+	return new Response(null, {
+		status: 204,
+		headers: {
+			'access-control-allow-methods': 'GET',
+			'access-control-allow-origin': '*',
+			'access-control-allow-headers': 'x-pronoundb-source',
+			'access-control-max-age': '600',
+		},
+	})
+}
+
+export function all () {
+	return new Response(JSON.stringify({ statusCode: 405, error: 'Method not allowed' }), { status: 405 })
+}
---
+++ packages/website/src/pages/api/v2/users/[id].ts
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) Cynthia Rey et al., All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import type { APIContext } from 'astro'
+
+import { ObjectId } from 'mongodb'
+import { ApiCallVersionCounter } from '@server/metrics.js'
+import { findById } from '@server/database/account.js'
+
+export async function get (ctx: APIContext) {
+	ApiCallVersionCounter.inc({ version: 2 })
+
+	if (!ctx.params.id || !ObjectId.isValid(ctx.params.id)) {
+		return new Response(
+			JSON.stringify({ code: 400, error: 'Bad request', message: 'Invalid user ID' }),
+			{
+				status: 400,
+				headers: {
+					'access-control-allow-methods': 'GET',
+					'access-control-allow-origin': '*',
+					'access-control-allow-headers': 'x-pronoundb-source',
+					'access-control-max-age': '600',
+					'content-type': 'application/json',
+				},
+			}
+		)
+	}
+
+	const id = new ObjectId(ctx.params.id)
+	const user = await findById(id)
+	if (!user) {
+		return new Response(
+			JSON.stringify({ code: 404, error: 'Not found' }),
+			{
+				status: 404,
+				headers: {
+					'access-control-allow-methods': 'GET',
+					'access-control-allow-origin': '*',
+					'access-control-allow-headers': 'x-pronoundb-source',
+					'access-control-max-age': '600',
+					'content-type': 'application/json',
+				},
+			}
+		)
+	}
+
+	const body = JSON.stringify({
+		id: ctx.params.id,
+		decoration: user.decoration,
+		sets: user.sets,
+	})
+
+	return new Response(body, {
+		headers: {
+			'access-control-allow-methods': 'GET',
+			'access-control-allow-origin': '*',
+			'access-control-allow-headers': 'x-pronoundb-source',
+			'access-control-max-age': '600',
+			'content-type': 'application/json',
+		},
+	})
+}
+
+export function options () {
+	return new Response(null, {
+		status: 204,
+		headers: {
+			'access-control-allow-methods': 'GET',
+			'access-control-allow-origin': '*',
+			'access-control-allow-headers': 'x-pronoundb-source',
+			'access-control-max-age': '600',
+		},
+	})
+}
+
+export function all () {
+	return new Response(JSON.stringify({ statusCode: 405, error: 'Method not allowed' }), { status: 405 })
+}
